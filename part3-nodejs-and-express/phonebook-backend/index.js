9a2b8181bf02c69b5800509684ccbd81231acca9
burakdiker <burak.diker@ug.bilkent.edu.tr>
2023-06-10 01:18:03 +0300

    Exercise 3.12. done

--- part3-nodejs-and-express/phonebook-backend/index.js
+++ part3-nodejs-and-express/phonebook-backend/index.js
@@ -9,7 +9,7 @@ morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
-app.use(express.static('build'))
+app.use(express.static('build'));
 
 let phonebook = [
   {
---
+++ part3-nodejs-and-express/phonebook-backend/mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+  console.log('give password as argument');
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://burakkddiker:${password}@cluster0.thl7lzy.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url);
+
+const contactSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+// const contact = new Contact({
+//   name: process.argv[3],
+//   number: process.argv[4],
+// });
+
+// contact.save().then((result) => {
+//   console.log(
+//     `added ${process.argv[3]} number ${process.argv[4]} to phonebook`
+//   );
+// });
+
+Contact.find({}).then((result) => {
+  console.log('phonebook: ');
+  result.forEach((note) => {
+    console.log(`${note.name} ${note.number}`);
+  });
+  mongoose.connection.close();
+});
