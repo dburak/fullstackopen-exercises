e126cee5e358b12973f3f9ec03d03c570f751ccc
burakdiker <burak.diker@ug.bilkent.edu.tr>
2023-06-16 22:24:57 +0300

    Exercises 3.13.-3.14 done

--- part3-nodejs-and-express/phonebook-backend/index.js
+++ part3-nodejs-and-express/phonebook-backend/index.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
+const Phonebook = require('./models/phonebook');
+const phonebook = require('./models/phonebook');
 
 app.use(express.json());
 app.use(cors());
@@ -11,31 +14,8 @@ app.use(
 );
 app.use(express.static('build'));
 
-let phonebook = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
-
 app.get('/api/persons', (req, res) => {
-  res.json(phonebook);
+  Phonebook.find({}).then((contacts) => res.json(contacts));
 });
 
 app.get('/api/persons/:id', (req, res) => {
@@ -55,23 +35,34 @@ app.delete('/api/persons/:id', (req, res) => {
 });
 
 app.post('/api/persons', (req, res) => {
-  const contact = req.body;
+  const body = req.body;
 
-  const id = Math.floor(Math.random() * 50);
-
-  const isNameExist = phonebook.find((p) => p.name === contact.name);
-
-  if (isNameExist) {
-    res.status(400).json({ error: 'name must be unique' });
-  } else if (!contact.name || !contact.number) {
-    res.status(400).json({ error: 'The name or number is missing' });
-  } else {
-    contact.id = id;
-
-    phonebook = phonebook.concat(contact);
-
-    res.status(201).json(phonebook);
+  if (body.name === undefined) {
+    return res.status(400).json({ error: 'content missing' });
   }
+
+  const phonebook = new Phonebook({
+    name: body.name,
+    number: body.number,
+  });
+
+  phonebook.save().then((savedContact) => res.json(savedContact));
+
+  // const id = Math.floor(Math.random() * 50);
+
+  // const isNameExist = phonebook.find((p) => p.name === contact.name);
+
+  // if (isNameExist) {
+  //   res.status(400).json({ error: 'name must be unique' });
+  // } else if (!contact.name || !contact.number) {
+  //   res.status(400).json({ error: 'The name or number is missing' });
+  // } else {
+  //   contact.id = id;
+
+  //   phonebook = phonebook.concat(contact);
+
+  //   res.status(201).json(phonebook);
+  // }
 });
 
 app.get('/info', (req, res) => {
