2928a7c73823b4cbcb87ebdfa94809165a32eff8
burakdiker <burak.diker@ug.bilkent.edu.tr>
2023-06-21 00:21:44 +0300

    Exercises 4.13.-4.14. doned

--- part4-testing-express-user-auth/blog-list/controllers/blogs.js
+++ part4-testing-express-user-auth/blog-list/controllers/blogs.js
@@ -9,7 +9,6 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const { title, url } = request.body;
 
-
   if (!title || !url) {
     response.status(400).end();
   } else {
@@ -17,6 +16,31 @@ blogsRouter.post('/', async (request, response) => {
 
     const result = await blog.save();
     response.status(201).json(result);
+  }
+});
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const result = await Blog.findByIdAndDelete(request.params.id);
+  result ? response.status(204).json(result) : response.status(404).end();
+});
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  const body = request.body;
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  };
+
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+      new: true,
+    });
+    response.json(updatedBlog);
+  } catch (error) {
+    next(error);
   }
 });
 
--- part4-testing-express-user-auth/blog-list/tests/blog-api.test.js
+++ part4-testing-express-user-auth/blog-list/tests/blog-api.test.js
@@ -29,26 +29,49 @@ test('verifies that the unique identifier property of the blog posts', async () 
   }
 });
 
-test('creates a new blog post', async () => {
-  const initialBlogs = await api.get('/api/blogs');
+describe('addition of a new note', () => {
+  test('creates a new blog post', async () => {
+    const initialBlogs = await api.get('/api/blogs');
 
-  await api.post('/api/blogs').send(newBlog).expect(201);
+    await api.post('/api/blogs').send(newBlog).expect(201);
 
-  const updatedBlogs = await api.get('/api/blogs');
-  expect(updatedBlogs.body.length).toBe(initialBlogs.body.length + 1);
+    const updatedBlogs = await api.get('/api/blogs');
+    expect(updatedBlogs.body.length).toBe(initialBlogs.body.length + 1);
+  });
+
+  test('verifies that if the likes property is missing from the request', async () => {
+    const result = await api.post('/api/blogs').send(newBlog).expect(201);
+
+    const createdBlog = result.body;
+
+    expect(createdBlog.likes).toBeDefined();
+    expect(createdBlog.likes).toBe(0);
+  });
+
+  test('verify that if the title or url properties are missing from the request data', async () => {
+    await api.post('/api/blogs').send(newBlog).expect(400);
+  });
 });
 
-test('verifies that if the likes property is missing from the request', async () => {
-  const result = await api.post('/api/blogs').send(newBlog).expect(201);
-
-  const createdBlog = result.body;
-
-  expect(createdBlog.likes).toBeDefined();
-  expect(createdBlog.likes).toBe(0);
+test('deleting a single blog post resource', async () => {
+  await api.delete('/api/blogs/6491d6f058b4a6ac689d2680');
 });
 
-test('verify that if the title or url properties are missing from the request data', async () => {
-  await api.post('/api/blogs').send(newBlog).expect(400);
+test('updating the information of an individual blog post', async () => {
+  const updatedBlog = {
+    title: 'Updated Title',
+    author: 'Burak Diker',
+    url: 'url',
+    likes: 2,
+  };
+
+  const result = await api
+    .put('/api/blogs/6491d6ed58b4a6ac689d267e')
+    .send(updatedBlog);
+
+  expect(result.status).toBe(200);
+
+  expect(result.body.likes).toBe(updatedBlog.likes);
 });
 
 afterAll(async () => {
