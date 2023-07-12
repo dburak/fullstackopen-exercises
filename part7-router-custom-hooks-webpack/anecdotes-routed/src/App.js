5965c46768dcd89c2e83bd96d7c126171be4a459
burakdiker <burak.diker@ug.bilkent.edu.tr>
2023-07-12 22:25:15 +0300

    Exercises 7.4.-7.8. done

--- part7-router-custom-hooks-webpack/anecdotes-routed/src/App.js
+++ part7-router-custom-hooks-webpack/anecdotes-routed/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Routes, Route, Link, useMatch, useNavigate } from 'react-router-dom';
+import { useField } from './hooks';
 
 const Menu = () => {
   const padding = {
@@ -85,18 +86,24 @@ const Footer = () => (
 );
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('');
-  const [author, setAuthor] = useState('');
-  const [info, setInfo] = useState('');
+  const content = useField('content');
+  const author = useField('author');
+  const info = useField('info');
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
+  };
+
+  const handleClear = () => {
+    content.onChange({ target: { value: '' } });
+    author.onChange({ target: { value: '' } });
+    info.onChange({ target: { value: '' } });
   };
 
   return (
@@ -105,29 +112,20 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name='content'
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name='author'
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name='info'
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
+        <button type='reset' onClick={handleClear}>
+          reset
+        </button>
       </form>
     </div>
   );
